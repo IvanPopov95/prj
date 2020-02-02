0f4d5fa17c779e28b7327116d65697704b71a298
IvanPopov95 <60357215+IvanPopov95@users.noreply.github.com>
2020-02-02 15:24:47 +0300

    Update conf.py

--- conf.py
+++ conf.py
@@ -8,49 +8,46 @@ def password_check(log, password):
     return res[0] == password
     
 
-def change_islogin(log):
-    islogin = islogin_select(log)
-    if islogin == 0:
-        cursor.execute("""UPDATE users SET islogin = 1 WHERE login = ?""", (log,))
-    else:
-        cursor.execute("""UPDATE users SET islogin = 0 WHERE login = ?""", (log,))
+def log_in(log):
+    res = (cursor.execute("""UPDATE users SET is_logged_in = 1
+WHERE login = ? AND is_logged_in = 0""", (log,)).rowcount)
     conn.commit()
+    return res > 0
+    
+
+def log_out(log):
+    res = (cursor.execute("""UPDATE users SET is_logged_in = 0
+WHERE login = ? AND is_logged_in = 1""", (log,)).rowcount)
+    conn.commit()
+    return res > 0
 
 
-def islogin_select(log):
-    cursor.execute("""SELECT islogin FROM users WHERE login = ?""", (log,))
-    res = cursor.fetchone()
-    return res[0]
-
+def login_internal(log, password):
+    if password_check(log, password):
+        if not log_in(log):
+            return 'You already login'
+    else:
+        return 'Wrong Password'
+            
 
 def login():
     log = input('login: ')
     password = input('password: ')
-    if password_check(log, password):
-        islogin = islogin_select(log)
-        if islogin == 0:
-            change_islogin(log)
-        else:
-            print('You already login')
-    else:
-        print('Wrong Password')
+    if login_internal(log, password) is not None:
+        print(login_internal(log, password))
 
 
 def logout():
     log = input('login: ')
-    islogin = islogin_select(log)
-    if islogin == 1:
-        change_islogin(log)
-    else:
+    if not log_out(log):
         print('You are not in system')
 
-  
-if __name__ == '__main__':
-    cursor = conn.cursor() 
-    conn = sqlite3.connect("mydatabase.db")
 
+if __name__ == '__main__':
+    conn = sqlite3.connect("mydatabase.db")
+    cursor = conn.cursor() 
     if sys.argv[1] == 'login':
         login()
     if sys.argv[1] == 'logout':
         logout()
-    conn.close()     
+    conn.close()
